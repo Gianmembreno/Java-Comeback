4532e4ac87d67ee2ab94cb0a3be2f9ba008f4164
Gian Membreno <gianmembreno@gmail.com>
2023-07-09 23:56:43 -0400

    subtract

--- test.java
+++ test.java
@@ -3,6 +3,11 @@ public class test {
     public static int add(int a, int b) {
         return a + b;
     }
+
+    //subtraction function
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
