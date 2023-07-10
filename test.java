f57fe3bb6cdb3e9369096646cd75922cc6051f7d
Gian Membreno <gianmembreno@gmail.com>
2023-07-09 23:57:21 -0400

    multiply

--- test.java
+++ test.java
@@ -3,13 +3,16 @@ public class test {
     public static int add(int a, int b) {
         return a + b;
     }
-
     //subtraction function
     public static int subtract(int a, int b) {
         return a - b;
     }
+
+    //multiplication function
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
     }
 }
